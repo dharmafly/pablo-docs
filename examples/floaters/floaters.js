77458b433c5e5dedd69f2d3032680a806a42fdfd
Premasagar Rose <p@dharmafly.com>
2012-08-05 23:01:09 +0100

    Move/add comments

--- examples/floaters/floaters.js
+++ examples/floaters/floaters.js
@@ -1,15 +1,5 @@
 'use strict';
 
-/*
-    TODO:
-    - convert Symbol to Symbolset or to Circleset
-    - explode into 4 squares on click, shoot off-screen never to return (a new Symbolset); worth many points; use same stroke-color as mother
-    - click within union between symbols, for double points
-    - keep score
-    - `panic` param -> greater velocity
-    - keep time; time is taken off score for that round; ticker sound, LCD font
-*/
-
 function createRoot(container){
     var root, width, height;
 
@@ -584,3 +574,15 @@ if (Pablo.isSupported && reqAnimFrame){
 else {
     alert('Sorry, your browser does not support the JavaScript technologies required for this demo.')
 }
+
+/////
+
+/*
+    TODO:
+    - explode into 4 squares on click, shoot off-screen never to return (a new Symbolset); worth many points; use same stroke-color as mother
+    - click within union between symbols, for double points
+    - keep score
+    - `panic` param -> greater velocity
+    - keep time; time is taken off score for that round; ticker sound, LCD font
+    - join symbol points, trigger second round with all circles recreated from the points in the last round
+*/
