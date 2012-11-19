65885bc3118dea95c45fd8f30f9cf26f5a353957
Premasagar Rose <p@dharmafly.com>
2012-11-19 13:08:15 +0000

    Basic app

---
+++ app/main.js
@@ -0,0 +1,19 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+	var screen = window.screen,
+		w = screen.availWidth,
+		h = screen.availHeight;
+
+	chrome.app.window.create(
+		'index.html',
+    	{
+    		width:  w,
+    		height: h
+    	}
+    );
+});
