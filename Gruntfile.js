5cf6f6076959de1faf3ee3fac1f567eb1a7cd354
Premasagar Rose <p@dharmafly.com>
2013-10-19 00:11:23 +0100

    Automate Grunt copy of HISTORY.md to misc/changelog

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,32 @@ module.exports = function(grunt) {
       copy: {
         main: {
           files: [
-            {expand:true, cwd:'../pablo/', src: ['pablo.js'], dest: 'docs/files/downloads/'},
-            {expand:true, cwd:'../pablo/build/', src: ['**'], dest: 'docs/files/downloads/'},
-            {expand:true, cwd:'../pablo/tests/', src: ['**', '!index.html', '!testrunner.js'], dest: 'docs/files/tests/'}
+            {
+              expand:true,
+              cwd:'../pablo/',
+              src: ['pablo.js'],
+              dest: 'docs/files/downloads/'
+            },
+            {
+              expand:true,
+              cwd:'../pablo/build/',
+              src: ['**'],
+              dest: 'docs/files/downloads/'
+            },
+            {
+              expand:true,
+              cwd:'../pablo/tests/',
+              src: ['**', '!index.html', '!testrunner.js'],
+              dest: 'docs/files/tests/'
+            },
+            {
+              expand:true,
+              cwd:'../pablo/',
+              src: ['HISTORY.md'],
+              rename: function(dest, src) {
+                return 'docs/misc/050. Changelog.md';
+              }
+            }
           ]
         }
       }
